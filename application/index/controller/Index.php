1610941bcf7dd788313a43f1a504c70b801732b0
QinWeilin <qinweilin0804@icloud.com>
2020-05-09 11:35:24 +0800

    2020.5.9.2

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,15 +7,6 @@ class Index extends Base
 {
     public function index()
     {
-        return $this->fetch();
-    }
-
-    public function hello($name = 'ThinkPHP5')
-    {
-        return 'hello,' . $name;
-    }
-
-    public function test(){
-        return $this->fetch('/test');
+        return $this->redirect('/index/dashboard');
     }
 }
